e4ba9b1f3c66a7892a06d2841fdee0d472032970
quandaworld <nhiquannguyen@gmail.com>
2025-03-15 15:10:34 -0700

    Add user schema

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new  mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  firstName: { 
+    type: String, 
+    required: true 
+  },
+  lastName: { 
+    type: String, 
+    required: true 
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+});
+
+// Method to pre-save hook to hash password
+userSchema.pre('save', function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = bcrypt.hashSync(this.password, 10);
+  }
+  next();
+});
+
+// Method to check if input password matches hashed password
+userSchema.methods.comparePassword = function (inputPassword) {
+  return bcrypt.compareSync(inputPassword, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
