7b3de8cea9ea44ff6a36cdbfd885d10a0c4aa678
quandaworld <nhiquannguyen@gmail.com>
2025-03-16 11:19:53 -0700

    Format code

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/auth/login');
-}
+  res.redirect("/auth/login");
+};
 
 module.exports = ensureAuthenticated;
