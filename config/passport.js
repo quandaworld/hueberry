9c33565ec80dc2dc46631726f9707700c2fcb174
quandaworld <nhiquannguyen@gmail.com>
2025-03-15 19:34:02 -0700

    Configure user authentication with LocalStrategy

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: "That username is not registered",
+            });
+          }
+
+          // Check if password is correct using comparePassword method
+          const isMatch = user.comparePassword(password);
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Password incorrect" });
+          }
+        })
+        .catch((err) => console.log(err));
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+};
