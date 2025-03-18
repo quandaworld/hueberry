2083a1568ebd7da2f638d053c27c8ec9004bd53c
quandaworld <nhiquannguyen@gmail.com>
2025-03-18 16:45:19 -0700

    Update parameter name to be more descriptive

--- utils/verifyOwnership.js
+++ utils/verifyOwnership.js
@@ -1,5 +1,5 @@
-exports.verifyOwnership = (asset, user, req, res, action) => {
-  if (asset.createdBy.toString() !== user._id.toString()) {
+exports.verifyOwnership = (resource, user, req, res, action) => {
+  if (resource.createdBy.toString() !== user._id.toString()) {
     req.flash("error", `You do not have permission to ${action} this visual`);
     res.redirect("/assets");
     return false;
