dd463f61297a3b4bc19ecb098a2bffddff033861
quandaworld <nhiquannguyen@gmail.com>
2025-03-18 13:34:10 -0700

    Format code with Prettier

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,19 @@
-document.getElementById('user-menu-button').addEventListener('click', function() {
-  const dropdown = document.getElementById('user-dropdown');
-  if (dropdown.style.display === 'none') {
-    dropdown.style.display = 'block';
+const userMenuButton = document.getElementById("user-menu-button");
+const userDropdown = document.getElementById("user-dropdown");
+
+// Functionality for the user dropdown menu
+userMenuButton.addEventListener("click", function () {
+  if (userDropdown.style.display === "none") {
+    userDropdown.style.display = "block";
   } else {
-    dropdown.style.display = 'none';
+    userDropdown.style.display = "none";
   }
 });
 
-document.querySelectorAll('.asset-card').forEach(card => {
-  card.addEventListener('click', function() {
-    const link = this.querySelector('a').getAttribute('href');
+// Close the dropdown if the user clicks outside of it
+document.querySelectorAll(".asset-card").forEach((card) => {
+  card.addEventListener("click", function () {
+    const link = this.querySelector("a").getAttribute("href");
     window.location.href = link;
   });
 });
