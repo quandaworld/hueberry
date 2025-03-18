9c086d0846d8ca660bf130309a184a4325a538a0
quandaworld <nhiquannguyen@gmail.com>
2025-03-18 12:18:24 -0700

    Connect and use method override middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 const flash = require("connect-flash");
+const methodOverride = require("method-override");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -21,6 +22,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // Configure session
 app.use(
