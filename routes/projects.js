0aec343658ecd8f9f1110e671d18e83f8377aa97
quandaworld <nhiquannguyen@gmail.com>
2025-03-18 22:57:24 -0700

    Set up project routers

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const projectController = require("../controllers/projectController");
+const ensureAuthenticated = require("../middleware/auth");
 
+// Ensure user is authenticated for all routes
+router.use(ensureAuthenticated);
+
+// New project form
+router.get('/new', projectController.getNewProjectForm);
+
+// Create project
+router.post('/', projectController.createProject);
+
+// Project details
+router.get('/:id', projectController.getProjectDetails);
+
+// Add asset to project
 router.post('/:projectId/assets/:assetId', projectController.addAssetToProject);
+
+// Remove asset from project
 router.delete('/:projectId/assets/:assetId', projectController.removeAssetFromProject);
+
+module.exports = router;
