840df33197a139cbe2ebfefde7ccd4a6746db2fd
quandaworld <nhiquannguyen@gmail.com>
2025-03-18 22:55:51 -0700

    Update project controller

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,68 @@
 const Project = require('../models/project');
 const { verifyOwnership } = require('../utils/verifyOwnership');
 
+// Display new project form
+exports.getNewProjectForm = (req, res) => {
+  res.render('projects/new');
+};
+
+// Create new project
+exports.createProject = async (req, res) => {
+  try {
+    // Get form data
+    const { name, description } = req.body;
+    
+    // Create new project
+    const project = new Project({
+      name,
+      description,
+      createdBy: req.user._id,
+      assets: []
+    });
+    
+    await project.save();
+    
+    req.flash('success', 'Project created successfully');
+    res.redirect(`/projects/${project._id}`);
+  } catch (error) {
+    console.error('Error creating project:', error);
+    res.render('projects/new', {
+      error: 'Error creating project: ' + error.message,
+      formData: req.body
+    });
+  }
+};
+
+// Get all projects for current user
+exports.getUserProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ createdBy: req.user._id }).sort({ updatedAt: -1 });
+    res.render('projects/index', { projects });
+  } catch (error) {
+    console.error('Error getting projects:', error);
+    req.flash('error', 'Error retrieving projects');
+    res.redirect('/dashboard');
+  }
+};
+
+// Get project details
+exports.getProjectDetails = async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id)
+      .populate('createdBy', 'firstName lastName')
+      .populate('assets');
+    
+    res.render('projects/show', {
+      project,
+      user: req.user
+    });
+  } catch (error) {
+    console.error('Error getting project details:', error);
+    req.flash('error', 'Error retrieving project');
+    res.redirect('/projects');
+  }
+};
+
 // Add asset to project
 exports.addAssetToProject = async (req, res) => {
   try {
