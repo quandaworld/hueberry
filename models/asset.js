876c92cad027f5d7c8dc181d4a5f4cfdf0951ef4
quandaworld <nhiquannguyen@gmail.com>
2025-03-18 17:08:26 -0700

    Format code with Prettier

--- models/asset.js
+++ models/asset.js
@@ -1,41 +1,36 @@
 const mongoose = require("mongoose");
 
-const assetSchema = new mongoose.Schema({
-  fileName: {
-    type: String,
-    required: true,
-  },
-  fileUrl: {
-    type: String,
-    required: true,
-  },
-  publicId: {
-    type: String,
-    required: true,
-  },
-  fileType: {
-    type: String,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  colors: [
-    {
-      rgb: [Number],
-      percentage: Number,
+const assetSchema = new mongoose.Schema(
+  {
+    fileName: {
+      type: String,
+      required: true,
     },
-  ],
-  tags: [String],
-});
+    fileUrl: {
+      type: String,
+      required: true,
+    },
+    publicId: {
+      type: String,
+      required: true,
+    },
+    fileType: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    colors: [
+      {
+        rgb: [Number],
+        percentage: Number,
+      },
+    ],
+    tags: [String],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Asset", assetSchema);
